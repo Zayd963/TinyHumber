619fd5743b94a2542733ce74848deee76b0a3f86
Zayd Qureshi <zayd.qureshi9@gmail.com>
2022-02-10 00:50:59 -0500

    test scene update

---
+++ TinyHumber/Assets/Scripts/Node.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool visited = false;
+    float globalGoal = 0;
+    float localGoal = 0;
+    Node[] connectedNodes;
+    Node parent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
